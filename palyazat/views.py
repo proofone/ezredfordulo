a29480bbc97264a10fb8fe9c410dcfb47604540d
prooF <proof87@gmail.com>
2024-03-06 23:18:42 +0100

    Palyazat views + templates

--- palyazat/views.py
+++ palyazat/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from palyazat.models import Kiiras
 
-# Create your views here.
+
+class KiirasListView(ListView):
+    model = Kiiras
+    template_name = "palyazat/list.html"
+
+
+class KiirasDetailView(DetailView):
+    model = Kiiras
+    template_name = "palyazat/palyazat_detail.html"
